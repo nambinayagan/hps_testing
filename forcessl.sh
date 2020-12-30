8b80afa288c14dc527c74077e9481f177e9c1236
Nambinayagan <75838920+nambinayagan@users.noreply.github.com>
2020-12-30 13:44:25 +0530

    Modified to work with server

--- forcessl.sh
+++ forcessl.sh
@@ -1,19 +1,30 @@
 #!/bin/bash/
+finddocroot()
+{
+        docroot1=$( /root/bin/ui $user1 | grep "Doc Root" | cut -d":" -f2 )
 
-docroot1="/home/nambinayagan.y/hom/atestuser/public_html"
-[ -z "$docroot1" ] && return;
-echo $docroot1
+        [ -z "$docroot1" ] && exit;
+}
+
+read -p "Enter username: " user1
+finddocroot
+#docroot1="/home/nambinayagan.y/hom/atestuser/public_html"
+#[ -z "$docroot1" ] && return;
+echo " Document root is $docroot1 "
 epooch=$( date +'%s' )
 echo $epooch
 #touch $docroot1/.htaccess
-echo adding >> $docroot1/.htaccess
-echo Before
+#echo adding >> $docroot1/.htaccess
+#echo Before
 cat $docroot1/.htaccess
 
-[ -z "$docroot1/.htaccess" ] &&  echo "htaccess absent"  ||  mv $docroot1/.htaccess $docroot1/.htaccess_$epooch;
+[ -f "$docroot1/.htaccess" ] && { mv $docroot1/.htaccess $docroot1/.htaccess_$epooch ; move=1 ; }
+echo -e "## Start Redirection code ##" >> $docroot1/.htaccess
+echo -e \n >> $docroot1/.htaccess
 echo -e "\nHeader always set Content-Security-Policy" \""upgrade-insecure-requests;\"""\nRewriteEngine On\nRewriteCond %{HTTPS} off\nRewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\n" >> $docroot1/.htaccess
-cat $docroot1/.htaccess_$epooch >> $docroot1/.htaccess
-echo -e \n
-echo After
+[[  $move -eq 1 ]]  && cat $docroot1/.htaccess_$epooch >> $docroot1/.htaccess ;
+#echo -e \n  >> $docroot1/.htaccess
+#echo After
+ls -al $docroot1/.htaccess_$epooch
 cat $docroot1/.htaccess
 #ls -al $docroot1 | grep htaccess

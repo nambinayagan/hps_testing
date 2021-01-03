5476c26aaf1cf4590330e3210bfe28ee8dc9d74d
Nambinayagan <75838920+nambinayagan@users.noreply.github.com>
2021-01-03 11:37:03 +0530

    Added certificate checking

---
+++ forcessl2.sh
@@ -0,0 +1,45 @@
+#!/bin/bash/
+domainpointing()
+{
+domainIP1=$( dig $domain1 +short)
+domainIP2=$( cat /var/cpanel/users/${username1##*( )} | grep "IP=" | cut -d'=' -f2 )
+[ $domainIP1 != $domainIP2 ] && { echo "$domain1 is pointing to $domainIP1 and not to hosting server IP address $domainIP2" ; exit ; } || { echo $domain1 is pointing to $domainIP2 ; }
+}
+sslchecker()
+{
+echo -e "\n"
+curl --insecure -vvI https://$domain1 2>&1 | awk 'BEGIN { cert=0 } /^\* Server certificate:/ { cert=1 } /^\*/ { if (cert) print }' | grep "Let's Encrypt" > /dev/null
+[[  $? -eq 0 ]] && { echo -e "\n\n" ; curl --insecure -vvI https://$domain1 2>&1 | awk 'BEGIN { cert=0 } /^\* Server certificate:/ { cert=1 } /^\*/ { if (cert) print }' ; echo -e "\n\n" ; } || { echo "SSL not instaalled" ; exit 1; }
+
+}
+checkredirection()
+{
+curl -iLs $domain1 | egrep 'HTTP|Location' | grep "https://" 2>&1 1>/dev/null
+[[  $? -eq 0 ]] && { echo "$domain1 is redirecting to a secure site" ; curl -iLs $domain1 | egrep 'HTTP|Location'; exit 1; }
+}
+finddocroot()
+{
+        docroot1=$( /root/bin/ui $domain1 | grep "Doc Root" | cut -d":" -f2 2> /dev/null )
+
+        [ -z "$docroot1" ] && exit;
+}
+
+read -p "Enter username: " domain1
+username1=$( /root/bin/ui $domain1 | grep "User:" | cut -d":" -f2 2> /dev/null )
+domainpointing
+finddocroot
+sslchecker
+checkredirection
+echo " Document root is $docroot1 "
+epooch=$( date +'%s' )
+echo Epoch time is $epooch
+[ -f $docroot1/.htaccess ] &&  cp $docroot1/.htaccess $docroot1/.htaccess_fssl_"$epooch"
+
+echo -e "\n ##Start Redirection Code## \n\nHeader always set Content-Security-Policy" \""upgrade-insecure-requests;\"""\nRewriteEngine On\nRewriteCond %{HTTPS} off\nRewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]\n ##End Redirection Code##\n$( cat $docroot1/.htaccess )" > $docroot1/.htaccess
+ls -al $docroot1/.htaccess*
+
+cat $docroot1/.htaccess
+echo -e "\n\n"
+
+curl -iLs $domain1 | egrep 'HTTP|Location'
+echo -e "\n\n"
